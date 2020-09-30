9536a3ea7e37447de0617239fc9110b494584dcc
Faun <Faun.schuetz@gmail.com>
2020-09-30 19:53:28 +0100

    Commit college exercises

---
+++ LabSheet4/Exercise1/TestComputer.java
@@ -0,0 +1,33 @@
+package LabSheet4.Exercise1;
+/* TestComputer.java
+ * By: Faun Schutz
+ * Start: 29/08/2020
+ * Finish: 29/08/2020
+ * */
+
+import javax.swing.*;
+
+public class TestComputer {
+    public static void main(String[] args) {
+        String output = "";
+
+        Computer c1 = new Computer();
+
+        output += "Calling the no-argument Computer constructor. " +
+                  "The first Computer object details are: \n\n" + c1.toString();
+
+        Computer c2 = new Computer("Dell","Laptop",3.25,16,550.99);
+
+        c2.setSpeed(2.75);
+        c2.setSpeed(-3.15);
+
+        output += "\n\nCalling the multi-argument Computer constructor. " +
+                  "The second Computer object details are: \n\n" + c2.toString();
+
+        JOptionPane.showMessageDialog(null,output,"Computer Object Data",
+                                            JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+    }
+
+}
