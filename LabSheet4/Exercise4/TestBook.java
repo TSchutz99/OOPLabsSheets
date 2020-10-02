63c67f7601516a48b0c6808d3ebebb2a51e4d405
Faun <Faun.schuetz@gmail.com>
2020-10-02 12:19:20 +0100

    Committing college exercises

--- LabSheet4/Exercise4/TestBook.java
+++ LabSheet4/Exercise4/TestBook.java
@@ -6,22 +6,35 @@ package LabSheet4.Exercise4;
  * */
 
 import javax.swing.*;
+import java.awt.Font;
 
 public class TestBook {
     public static void main(String[] args){
-        String output = " ";
+        JTextArea textArea = new JTextArea(5, 5);
+
+        Font textAreaFont = new Font("monospaced", Font.PLAIN, 12);
+        textArea.setFont(textAreaFont);
+
+        textArea.setText(String.format("%-15s%-10s%-20s%5s", "Title", "Price", "ISBN", "Pages"));
 
         Book b1 = new Book();
 
-        output += "\n\nCalling the no argument Book constructor. " +
-                  "The first Book object details are: \n\n" + b1.toString();
+        Book b2 = new Book();
 
-        Book b2 = new Book("The Da Vinci Code", 19.99, "3452617232", 348);
+        b1.setTitle(JOptionPane.showInputDialog("Please enter the title of your favourite book"));
+        b1.setPrice(Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of your favourite book")));
+        b1.setISBN(JOptionPane.showInputDialog("Please enter the ISBN of your favourite book"));
+        b1.setPages(Integer.parseInt(JOptionPane.showInputDialog("Please enter the pages of your favourite book")));
 
-        output += "\n\nCalling the multi-argument Book constructor. " +
-                  "The second Book object details are: \n\n" + b2.toString();
+        textArea.append(String.format("\n\n%-15s%-10.2f%-20s%5d", b1.getTitle(), b1.getPrice(), b1.getISBN(), b1.getPages()));
 
-        JOptionPane.showMessageDialog(null, output, "Book Object Data",
-                                        JOptionPane.INFORMATION_MESSAGE);
+        b2.setTitle(JOptionPane.showInputDialog("Please enter the title of your least favourite book"));
+        b2.setPrice(Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of your least favourite book")));
+        b2.setISBN(JOptionPane.showInputDialog("Please enter the ISBN of your least favourite book"));
+        b2.setPages(Integer.parseInt(JOptionPane.showInputDialog("Please enter the pages of your least favourite book")));
+
+        textArea.append(String.format("\n\n%-15s%-10.2f%-20s%5d", b2.getTitle(), b2.getPrice(), b2.getISBN(), b2.getPages()));
+
+        JOptionPane.showMessageDialog(null, textArea, "Book Object Data", JOptionPane.PLAIN_MESSAGE);
     }
 }
