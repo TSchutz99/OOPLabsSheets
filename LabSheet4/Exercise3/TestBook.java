e971092cdb89d0322685bb0ad56fa20336b0fb59
Faun <Faun.schuetz@gmail.com>
2020-10-02 11:39:10 +0100

    Committing college exercises

---
+++ LabSheet4/Exercise3/TestBook.java
@@ -0,0 +1,26 @@
+package LabSheet4.Exercise3;
+/* Book.java
+ * By: Faun Schutz
+ * Start: 02/09/2020
+ * Finish: 02/09/2020
+ * */
+import javax.swing.*;
+
+public class TestBook {
+    public static void main(String[] args){
+        String output = " ";
+
+        Book b1 = new Book();
+
+        output += "\n\nCalling the no argument Book constructor. " +
+                  "The first Book object details are: \n\n" + b1.toString();
+
+        Book b2 = new Book("The Da Vinci Code", 19.99, "3452617232", 348);
+
+        output += "\n\nCalling the multi-argument Book constructor. " +
+                  "The second Book object details are: \n\n" + b2.toString();
+
+        JOptionPane.showMessageDialog(null, output, "Book Object Data",
+                                        JOptionPane.INFORMATION_MESSAGE);
+    }
+}
