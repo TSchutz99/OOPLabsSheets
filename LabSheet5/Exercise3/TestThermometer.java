d54db7ff580a7524845431e586a76dbf55caf150
Faun <Faun.schuetz@gmail.com>
2020-10-06 18:22:05 +0100

    Committing college exercises

--- LabSheet5/Exercise3/TestThermometer.java
+++ LabSheet5/Exercise3/TestThermometer.java
@@ -1,4 +1,25 @@
 package LabSheet5.Exercise3;
 
-public class TestThermometer {
+import javax.swing.JOptionPane;
+
+public class TestThermometer{
+    public static void main(String[] args){
+        Thermometer T1 = new Thermometer(35);
+
+        Thermometer T2 = new Thermometer();
+
+        Thermometer T3 = new Thermometer(25);
+
+        JOptionPane.showMessageDialog(null,"******Thermometer Testing******" +
+                                "\n\nCalling the single-argument constructor .... setting the temperature of first thermometer to 35C" +
+                                "\nFirst Thermometer:\nCurrent temperature: " + T1.getCurrentTemp() +
+                                "\nMaximum temperature: " + T1.getMaximumTemp() + "\nMinimum temperature: " + T1.getMinimumTemp() +
+                                "\n\nCalling the single-argument constructor .... setting the temperature of second thermometer to 0C" +
+                                "\nSecond Thermometer:\nCurrent temperature: " + T2.getCurrentTemp() +
+                                "\nMaximum temperature: " + T2.getMaximumTemp() + "\nMinimum temperature: " + T2.getMinimumTemp() +
+                                "\n\nCalling the single-argument constructor .... setting the temperature of third thermometer to 25C" +
+                                "\nThird Thermometer:\nCurrent temperature: " + T3.getCurrentTemp() +
+                                "\nMaximum temperature: " + T3.getMaximumTemp() + "\nMinimum temperature: " + T3.getMinimumTemp(),
+                            " ", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
