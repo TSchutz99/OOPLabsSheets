f70ed894b9eac14b7c59ce60fe881a55ff16eb16
Faun <Faun.schuetz@gmail.com>
2020-10-07 18:52:51 +0100

    Committing college exercises

---
+++ LabSheet5/Exercise5/TestBankAccount.java
@@ -0,0 +1,27 @@
+package LabSheet5.Exercise5;
+/* TestBankAccount.java
+ * By: Faun Schutz
+ * Start: 07/09/2020
+ * Finish: 07/09/2020
+ * */
+
+import javax.swing.*;
+
+public class TestBankAccount{
+    public static void main(String[] args){
+        String output = "";
+
+        BankAccount account1 = new BankAccount();
+
+        BankAccount account2 = new BankAccount("Richy Rich",2342343,0.75);
+
+        output += "Calling the no-argument constructor. The first bank account details are: \n\n" + account1.toString() +
+                  "\n\nCalling the multi-argument constructor. The second bank account details are: \n\n" + account2.toString() +
+                  "\n\nNow we are calling the setInterestRate() method to change the interest rate to 0.5.";
+        BankAccount.setInterestRate(0.5);
+        output += "\n\nThe first bank account details are: \n\n" + account1.toString() +
+                  "\n\nThe second bank account details are: \n\n" + account2.toString();
+
+        JOptionPane.showMessageDialog(null, output," ", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
