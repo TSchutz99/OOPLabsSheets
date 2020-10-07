608b39611bc23f2adc3f571839c7b30c891bdab5
Faun <Faun.schuetz@gmail.com>
2020-10-07 18:48:49 +0100

    Committing college exercises

--- LabSheet5/Exercise4/TestBankAccount.java
+++ LabSheet5/Exercise4/TestBankAccount.java
@@ -17,8 +17,7 @@ public class TestBankAccount{
         output += "Calling the no-argument constructor. The first bank account details are: \n\n" + account1.toString() +
                   "\n\nCalling the multi-argument constructor. The second bank account details are: \n\n" + account2.toString() +
                   "\n\nNow we are calling the setInterestRate() method to change the interest rate to 0.5.";
-        account1.setInterestRate(0.5);
-        account2.setInterestRate(0.5);
+        BankAccount.setInterestRate(0.5);
         output += "\n\nThe first bank account details are: \n\n" + account1.toString() +
                   "\n\nThe second bank account details are: \n\n" + account2.toString();
 
