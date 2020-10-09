42c7e8f207f3185b43f48932910d0cf73e06d964
Faun <Faun.schuetz@gmail.com>
2020-10-09 13:36:35 +0100

    Committing college exercises

--- LabSheet6/Exercise1/TestPerson.java
+++ LabSheet6/Exercise1/TestPerson.java
@@ -4,6 +4,26 @@ package LabSheet6.Exercise1;
  * Start: 09/09/2020
  * Finish: 09/09/2020
  * */
+import javax.swing.*;
 
 public class TestPerson{
+    public static void main(String[] args){
+        String output = "";
+
+        Person Person1 = new Person();
+
+        output += "******Person Class Tester******" +
+                  "\n\nCalling the Person() constructor...." +
+                  "\nValue of the first Peron object is : " + Person1.toString();
+
+        String firstName = JOptionPane.showInputDialog("Please enter first name of the second person");
+        String lastName = JOptionPane.showInputDialog("Please enter last name of the second person");
+
+        Person Person2 = new Person(firstName, lastName);
+
+        output += "\n\nCalling the Person(String, String) constructor after getting user supplied values...." +
+                  "\nValue of the second Peron object is : " + Person2.toString();
+
+        JOptionPane.showMessageDialog(null, output, " ", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
