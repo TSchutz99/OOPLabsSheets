04d55a1a7ca2b07266ddc153b4357ad519cd93c3
Faun <Faun.schuetz@gmail.com>
2020-10-13 18:47:12 +0100

    Committing college exercises

---
+++ LabSheet7/Exercise3/TestCollegeApp.java
@@ -0,0 +1,11 @@
+package LabSheet7.Exercise3;
+/* TestCollegeApp.java
+ * By: Faun Schutz
+ * Start: 13/09/2020
+ * Finish: /09/2020
+ */
+public class TestCollegeApp {
+    public static void main(String[] args){
+
+    }
+}
