51a06544220a11107809c532a17f32cada79c3fa
Faun <Faun.schuetz@gmail.com>
2020-10-13 17:22:47 +0100

    Committing college exercises

---
+++ LabSheet7/Exercise1/Product.java
@@ -0,0 +1,50 @@
+package LabSheet7.Exercise1;
+/* Product.java
+ * By: Faun Schutz
+ * Start: 09/09/2020
+ * Finish: 09/09/2020
+ *
+ * A simple instantiable class definition for a Product. */
+
+public class Product {
+        private int id;
+        private String name;
+        private String description;
+        private static int count = 1000000;
+
+        public Product(String name, String description) {
+            setId(id);
+            setName(name);
+            setDescription(description);
+            incrementCount();
+        }
+
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = count+1;
+        }
+        private static void incrementCount(){
+            count++;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String toString() {
+            return "Product details are as follows: id: " + getId() + "  name: " + getName() + "  description: " + getDescription();
+        }
+}
+
