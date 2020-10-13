458d0ecb0e5bbd7d1d69c5fe174f268f54c094c1
Faun <Faun.schuetz@gmail.com>
2020-10-13 18:52:19 +0100

    Committing college exercises

--- LabSheet7/Exercise3/TestCollegeApp.java
+++ LabSheet7/Exercise3/TestCollegeApp.java
@@ -6,6 +6,10 @@ package LabSheet7.Exercise3;
  */
 public class TestCollegeApp {
     public static void main(String[] args){
+        Students student1 = new Students(154345, "Jake", "Computing");
+        Students student2 = new Students(234532, "Mary", "Creative Media");
+        Students student3 = new Students(453726, "Tommy", "Computing");
+        Students student4 = new Students(623456, "Peter", "Creative Media");
 
     }
 }
