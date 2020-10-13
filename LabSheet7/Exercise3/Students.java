8b55cfdd9c29938d680013a80448f9aaaba96dc5
Faun <Faun.schuetz@gmail.com>
2020-10-13 18:03:53 +0100

    Committing college exercises

--- LabSheet7/Exercise3/Students.java
+++ LabSheet7/Exercise3/Students.java
@@ -8,4 +8,35 @@ public class Students {
     private int id;
     private String name;
     private String department;
+
+    public Students(int id, String name, String department){
+        setId(id);
+        setName(name);
+        setDepartment(department);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public String getDepartment(){
+        return department;
+    }
+
+    public String toString(){
+        return "Name: " + getName() + "  ID: " + getId() + "  Department: " + getDepartment();
+    }
 }
