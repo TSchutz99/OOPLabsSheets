627dae656d3db0009a409e81c632d9f1e7fca359
Faun <Faun.schuetz@gmail.com>
2020-11-04 19:19:52 +0000

    Committing college exercises - No Problems

--- LabSheet10/sampleprogram1/TestInterface.java
+++ LabSheet10/sampleprogram1/TestInterface.java
@@ -1,10 +1,14 @@
 package LabSheet10.sampleprogram1;
-
-//TestInterface.java
-/*A driver program that tests the functionality of the Student, UnderGraduate and
-PostGraduate classes as well as the Person interface*/
+/* TestInterface.java
+ * By: Faun Schutz
+ * Start: 04/11/2020
+ * Finished: 04/11/2020
+ *
+ * A driver program that tests the functionality of the Student, UnderGraduate and
+ * PostGraduate classes as well as the Person interface. */
 
 import LabSheet10.exercise1.PermanentLecturer;
+import LabSheet10.exercise1.TemporaryLecturer;
 
 import java.util.*;
 
@@ -35,5 +39,15 @@ public class TestInterface {
 		 PermanentLecturer pl2 = new PermanentLecturer("Uncle Bob Martin", "Dublin", new GregorianCalendar(1052,04,19), "t0044356", new String[]{"Agile Methods", "OO Analysis & Design", "Design Patterns"},
 				                                        new GregorianCalendar(1986,03,18), "Oodles of Pension Entitlements :-)", "Part-time Senior Research Fellow");
 		 System.out.println(pl2);
+
+		 System.out.println("\n\nTesting the TemporaryLecturer multi-arg constructor: ");
+		 TemporaryLecturer tl1 = new TemporaryLecturer("Richard O' Neill", "Tallaght, Dublin", new GregorianCalendar(1985,03,04), "3536278b", new String[]{"Applied Biology", "Microbiology"},
+				                                        new GregorianCalendar(2018,04,15), 762);
+		 System.out.println(tl1);
+
+		 System.out.println("\n\nTesting the TemporaryLecturer multi-arg constructor: ");
+		 TemporaryLecturer tl2 = new TemporaryLecturer("Mary McKenzie", "Bundoran, Donegal", new GregorianCalendar(1971,06,13), "647382", new String[]{"Fractal Geometry", "Tensors"},
+				 new GregorianCalendar(2016,03,8), 1342);
+		 System.out.println(tl2);
      }
 }
